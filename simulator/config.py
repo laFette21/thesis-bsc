7c06155bddbd520dd1ba838f6a45d0fcbf4dec6e
laFette21 <viragh.akos21@gmail.com>
2022-01-22 13:44:11 +0100

    Config added

---
+++ simulator/config.py
@@ -0,0 +1,8 @@
+CONE_DISTANCE = 5  # m
+DATA_DENSITY = 20000  # interpolated data between points
+FIELD_OF_VIEW = 180  # degrees
+ODOMETRY_SAMPLING = 5  # ms
+PERCEPTION_SAMPLING = 50  # ms
+RANGE_OF_VIEW = 15  # m
+TRACK_HALF_WIDTH = 1.5  # m
+VELOCITY = 5.0  # m/s
