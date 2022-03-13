48280c15e7d86ee7a5385123f63ab8dcfbfa9b59
laFette21 <viragh.akos21@gmail.com>
2022-03-13 20:55:54 +0100

    Odometry type added and minor refactor

--- slam_cpp/include/types.h
+++ slam_cpp/include/types.h
@@ -7,28 +7,31 @@
 
 #include "utils.h"
 
-struct Pose
+struct Odometry
 {
-    double m_data[3];
+    double data[2];
 
-    Pose(double x = 0, double y = 0, double psi = 0): m_data{x, y, psi} {}
+    Odometry(double velocity = 0, double angular_velocity = 0): data{velocity, angular_velocity} {}
 
-    Pose operator+(const Pose& obj) { return Pose(m_data[0] + obj.m_data[0], m_data[1] + obj.m_data[1], m_data[2] + obj.m_data[2]); }
-    Pose operator*(const Pose& obj)
-    {
-        Eigen::Matrix2d R1 = RotationMatrix2D<double>(m_data[2]);
-        Eigen::Matrix2d R2 = RotationMatrix2D<double>(obj.m_data[2]);
-        Eigen::Matrix2d R3 = R2 * R1;
-        Eigen::Vector2d pos1 = {m_data[0], m_data[1]};
-        Eigen::Vector2d pos2 = {obj.m_data[0], obj.m_data[1]};
-        Eigen::Vector2d pos3 = pos1 + R1.transpose() * pos2;
-
-        return Pose(pos3[0], pos3[1], NormalizeAngle<double>(ceres::atan2(R3(1, 0), R3(0, 0))));
-    }
-    friend std::ostream& operator<<(std::ostream& os, const Pose& pose)
+    friend std::ostream& operator<<(std::ostream& os, const Odometry& obj)
     {
         os << std::setprecision(16);
-        os << pose.m_data[0] << ' ' << pose.m_data[1] << ' ' << pose.m_data[2];
+        os << obj.data[0] << ' ' << obj.data[1];
+
+        return os;
+    }
+};
+
+struct Pose
+{
+    double data[3];
+
+    Pose(double x = 0, double y = 0, double psi = 0): data{x, y, psi} {}
+
+    friend std::ostream& operator<<(std::ostream& os, const Pose& obj)
+    {
+        os << std::setprecision(16);
+        os << obj.data[0] << ' ' << obj.data[1] << ' ' << obj.data[2];
 
         return os;
     }
@@ -36,18 +39,18 @@ struct Pose
 
 struct Landmark
 {
-    double m_distance;
-    double m_bearing;
-    int m_color;
-    int m_id;
+    double distance;
+    double bearing;
+    int color;
+    int id;
 
     Landmark(int id = -1, double distance = 0, double bearing = 0, int color = 0):
-        m_id(id), m_distance(distance), m_bearing(bearing), m_color(color) {}
+        id(id), distance(distance), bearing(bearing), color(color) {}
 
-    friend std::ostream& operator<<(std::ostream& os, const Landmark& lm)
+    friend std::ostream& operator<<(std::ostream& os, const Landmark& obj)
     {
         os << std::setprecision(16);
-        os << lm.m_id << ' ' << lm.m_distance << ' ' << lm.m_bearing << ' ' << lm.m_color;
+        os << obj.id << ' ' << obj.distance << ' ' << obj.bearing << ' ' << obj.color;
 
         return os;
     }
