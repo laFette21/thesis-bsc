c1cd863d9ea2bc329fa0442a416e9e7c0db15807
laFette21 <viragh.akos21@gmail.com>
2022-05-03 18:38:37 +0200

    Update TestGraph.cpp

--- slam_cpp/src/graph/test/TestGraph.cpp
+++ slam_cpp/src/graph/test/TestGraph.cpp
@@ -14,4 +14,44 @@ TEST_CASE("Test Graph", "[graph]")
         REQUIRE(graph.getUniqueLandmarks().size() == 0);
         REQUIRE(*graph.getPoses().begin()->second == Pose());
     }
+
+    SECTION("Creating a Pose with Motion(0, 0)")
+    {
+        Graph graph;
+        const auto motion = std::make_shared<Motion>(0, 0);
+        graph.createPose(motion);
+
+        REQUIRE(graph.getLastId() == 2);
+        REQUIRE(graph.getPoses().size() == 2);
+        REQUIRE(graph.getUniqueLandmarks().size() == 0);
+        REQUIRE(*graph.getPoses().begin()->second == Pose());
+        REQUIRE(*graph.getPoses().rbegin()->second == Pose());
+    }
+
+    SECTION("Creating a Pose with Motion(1, 1)")
+    {
+        Graph graph;
+        const auto motion = std::make_shared<Motion>(1, 1);
+        graph.createPose(motion);
+
+        REQUIRE(graph.getLastId() == 2);
+        REQUIRE(graph.getPoses().size() == 2);
+        REQUIRE(graph.getUniqueLandmarks().size() == 0);
+        REQUIRE(*graph.getPoses().begin()->second == Pose());
+        REQUIRE_FALSE(*graph.getPoses().rbegin()->second == Pose());
+    }
+
+    SECTION("Creating a Landmark")
+    {
+        Graph graph;
+        std::shared_ptr<std::vector<std::shared_ptr<Perception>>> perceptions(new std::vector<std::shared_ptr<Perception>>());
+        perceptions->push_back(std::shared_ptr<Perception>(new Perception(0, 0, 0, 0, 0, 0)));
+        graph.createLandmark(perceptions);
+
+        REQUIRE(graph.getUniqueLandmarks().size() == 1);
+
+        graph.createLandmark(perceptions);
+        REQUIRE(graph.getUniqueLandmarks().size() == 1);
+    }
+
 }
