23e77011d59aa63b07f3c7d1cb28278e6a3caa13
laFette21 <viragh.akos21@gmail.com>
2022-05-09 22:11:30 +0200

    Update coverage.sh

--- slam_cpp/tools/coverage.sh
+++ slam_cpp/tools/coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcovr --html-details coverage.html -r .. -e ".*Test*" -e ".*catch.hpp" -e ".*argparse.hpp" -e ".*matplotlibcpp.h" -e ".*main.cpp"
+gcovr --exclude-unreachable-branches --html-details coverage.html -r .. -e ".*Test*" -e ".*catch.hpp" -e ".*argparse.hpp" -e ".*matplotlibcpp.h" -e ".*main.cpp"
